a619111b9f061c2705349d162cb082fa288149c8
drawy <ahmedhanfy1111@gmail.com>
2019-03-12 06:54:30 +0200

    khaled's edit

--- Assignment 2/V1.0.cpp
+++ Assignment 2/V1.0.cpp
@@ -1,33 +1,19 @@
-/** Version: 1.0 - March 10th 2019
-*   Author(s): Ahmad Khaled Fawzy
+/** Version: 1.0 - March 12th 2019
+*   Author(s): Ahmad Khaled Fawzy - 20170377
 *              Ahmad Mohammad Hanafy
 */
 
 ///NOTE: DO NOT CREATE BOOKS MANUALLY, read books from user using cin.getline to append \0 instead.
 ///NOTE: all seeking operations will be shifted by sizeof(short) because the first 2 bytes of the file contain the current head
+///NOTE: RRN is 0 based throughout the whole program
 
 
 /**
-*   we have the same output files when we use the >> operator and the input in the main without the cin.ignore i commented it
-*   we need to check how to delete the record
-*   to make compaction write all file in temp file and rewrite it back to the original one
-*   we need to read the head from the file itself
-*
-*
-*/
-
-/** 
- *  DONE        1-  Delete book (given the ISBN), you are asked to use the avail list technique for fixed length
- *                  records, so that you can reuse the deleted records in new insertions. The list head is initially
- *                  assigned ‐1, its data type is: short, and stored at the beginning of the file.
- * TODO         2- Add book, you must check first if there is a deleted record in the available list or not, if the
+ *  TODO         3- PRINT ALL BOOOKS
+ *  TODO         4 - COMPACT FILs
+ *  DONE/NOT-TESTED 5- Add book, you must check first if there is a deleted record in the available list or not, if the
  *                  list head is = ‐1, this means that you should append the new record.
- * TODO         3 - UPDATE BOOK
- *
- *  DONE         4 - PRINT BOOK GIVEN isbn
- *  TODO         5- PRINT ALL BOOOKS
- *  TODO         6 - COMPACT FILE
- *
+ *  TODO         6 - UPDATE BOOK
  */
 #include <bits/stdc++.h>
 #include <fstream>
@@ -42,63 +28,92 @@ struct Book
     char ISBN[6]; /// 5 chars for ISBN, the 6th char is for \0
     char Title[60];
     char Author[60];
-    double  Price;          /// i need to change those to chars, why doubles and int ??,  i used them anyway
+    double  Price;
     int  Year;
     int  nPages;
-    int RRN ;
-    friend istream&operator >> (istream & in , Book &obj ){
-
+    friend istream&operator >> (istream & in , Book &obj ) ///TODO handle cascaded input by sitting ignore() flag.
+    {
         in.getline( obj.ISBN , sizeof(obj.ISBN));
         in.getline(obj.Author , sizeof(obj.Author));
         in.getline(obj.Title , sizeof(obj.Title));
         in>>obj.Price>>obj.Year>>obj.nPages;
         return in ;
     }
-    friend ostream &operator << (ostream & out  , Book &obj){
-        out<<"ISBN :"<<obj.ISBN<<"\n";
-        out<<"Author :"<<obj.Author<<"\n";
-        out<<"Title :"<<obj.Title<<"\n";
-        out<<"Price :"<<obj.Price<<"$\n";
-        out<<"pages :"<<obj.Year<<"\n";
+    friend ostream &operator << (ostream & out  , Book &obj)
+    {
+        out<<"ISBN: " << obj.ISBN<<"\n";
+        out<<"Author: " << obj.Author<<"\n";
+        out<<"Title: " << obj.Title<<"\n";
+        out<<"Price: " << obj.Price<<"$\n";
+        out<<"Year: " << obj.Year << "\n";
+        out<<"Pages: " << obj.nPages<<"\n";
         return  out ;
     }
 };
 /** to add to the middle of the file we need to provide a byteoffset
 *  update : i made the byteoffset to make the function work abstractly and we handle the deletion and insertion position somewhere else, Hanafy
 */
-void addBook (fstream &out, Book &someBook, long long BYTEOFF = ios::end) ///TODO handle the case of writing at the current AVAIL_LIST head instead of appending at the end of the file
+void addBook (fstream &recordsFile, Book &someBook)
 {
-//    out.seekp(BYTEOFF ,  ios:: beg); ///todo check this again
+    short currentHead;
+    recordsFile.seekg(0, ios::beg);
+    recordsFile.read((char*) &currentHead, sizeof(currentHead));
     char delimiter = '#';
-    ///if no deleted records exist (head = -1)
-        ///write at the end of the file
-        out.seekp(0,ios::end);
+    if (currentHead == -1) /// if there exists no deleted records then append the new record at the end of the file
+    {
+        recordsFile.seekp(0,ios::end);
 
-        out.write(someBook.ISBN, sizeof(someBook.ISBN));
-        out.write(&delimiter, sizeof(delimiter));
+        recordsFile.write(someBook.ISBN, sizeof(someBook.ISBN));
+        recordsFile.write(&delimiter, sizeof(delimiter));
 
-        out.write(someBook.Title, sizeof(someBook.Title));
-        out.write(&delimiter, sizeof(delimiter));
+        recordsFile.write(someBook.Title, sizeof(someBook.Title));
+        recordsFile.write(&delimiter, sizeof(delimiter));
 
-        out.write(someBook.Author, sizeof(someBook.Author));
-        out.write(&delimiter, sizeof(delimiter));
+        recordsFile.write(someBook.Author, sizeof(someBook.Author));
+        recordsFile.write(&delimiter, sizeof(delimiter));
 
-        out.write((char*)&someBook.Price, sizeof(someBook.Price));
-        out.write(&delimiter, sizeof(delimiter));
+        recordsFile.write((char*)&someBook.Price, sizeof(someBook.Price));
+        recordsFile.write(&delimiter, sizeof(delimiter));
 
-        out.write((char*)&someBook.Year, sizeof(someBook.Year));
-        out.write(&delimiter, sizeof(delimiter));
+        recordsFile.write((char*)&someBook.Year, sizeof(someBook.Year));
+        recordsFile.write(&delimiter, sizeof(delimiter));
 
-        out.write((char*)&someBook.nPages, sizeof(someBook.nPages));
-        out.write(&delimiter, sizeof(delimiter));
+        recordsFile.write((char*)&someBook.nPages, sizeof(someBook.nPages));
+        recordsFile.write(&delimiter, sizeof(delimiter));
+    }
+    else
+    {
+        short stuffedHead;
+        recordsFile.seekg( (2 + (currentHead) * 148 + 1), ios::beg); /// seek the byte-offset of the latest deleted book, (+1) to skip '*' while reading
+        recordsFile.read((char*) &stuffedHead, sizeof(stuffedHead));
+        recordsFile.seekp(0, ios::beg); ///update AVAIL_LIST head
+        recordsFile.write((char*) &stuffedHead, sizeof(stuffedHead));
+        recordsFile.seekp( 2 + (currentHead)* 148); /// seek the byte-offset of the deleted book to write in its place
 
-    ///else
-        ///write at the current head
+        recordsFile.write(someBook.ISBN, sizeof(someBook.ISBN));
+        recordsFile.write(&delimiter, sizeof(delimiter));
+
+        recordsFile.write(someBook.Title, sizeof(someBook.Title));
+        recordsFile.write(&delimiter, sizeof(delimiter));
+
+        recordsFile.write(someBook.Author, sizeof(someBook.Author));
+        recordsFile.write(&delimiter, sizeof(delimiter));
+
+        recordsFile.write((char*)&someBook.Price, sizeof(someBook.Price));
+        recordsFile.write(&delimiter, sizeof(delimiter));
+
+        recordsFile.write((char*)&someBook.Year, sizeof(someBook.Year));
+        recordsFile.write(&delimiter, sizeof(delimiter));
+
+        recordsFile.write((char*)&someBook.nPages, sizeof(someBook.nPages));
+        recordsFile.write(&delimiter, sizeof(delimiter));
+    }
 }
-Book  readBook(fstream &out , short RRN){
-    long long byte =2+ RRN * sizeof(Book) + 6 * RRN;
+Book  readBook(fstream &out, short RRN)
+{
+    long long byte = 2 + (RRN) * 148; /// 148 is the size of each record (including all delimiters) in the file
     out.seekg(byte, ios::beg);
-    assert(!(out.end >byte));       /// asserting if the RRN of the desired book outside the file scope
+    assert(!(out.end > byte));       /// asserting if the RRN of the desired book outside the file scope
     Book temp;
     char delim ;
     out.read(temp.ISBN , sizeof(temp.ISBN));
@@ -114,71 +129,66 @@ Book  readBook(fstream &out , short RRN){
     out.read((char *) &temp.nPages, sizeof(temp.nPages));
     out.read(&delim, sizeof(delim));
     return temp;
-
 }
-void printBook(fstream &out, char isbn[]){
-    for (short rrn = 0; ; ++rrn) {
-      Book temp = readBook(out, rrn);
-      if(strcmp(temp.ISBN , isbn) ==0){
-          cout<<temp;
-          break;
-      }
+void printBook(fstream &out, char isbn[])
+{
+    for (short rrn = 0; ; ++rrn)
+    {
+        Book temp = readBook(out, rrn);
+        if(strcmp(temp.ISBN , isbn) == 0)
+        {
+            cout<<temp;
+            break;
+        }
     }
 }
-void updateAVAIL(fstream &out , short nOFF){/// not working yet
-    out.seekg(0 , ios::beg);
-    short temp = 0;
-    out.read((char *) &temp, sizeof(temp) );    /// reading probabely
-    cout<<"inside the update ; "<<temp<<endl;
-    out.seekp(0 , ios::beg);
-    out.write((char *)&nOFF , sizeof(nOFF));
-    out.seekp(2 +  nOFF * sizeof(Book)  + 6*nOFF *sizeof(char) + sizeof(char) , ios::beg); /// handling the * of deletion
-    out.write((char *)&temp , sizeof(temp));
-    return;
-}
 
-void deleteBook(fstream & out, char isbn[]){
+void deleteBook(fstream &recordsFile, char isbn[])
+{
     short RRN = 0 ;
-    while(true){
-        out.seekg(2 + RRN * sizeof(Book) + 6*RRN* sizeof(char) ,ios::beg);///2 for the head + RRN *sizeof record + 6 delimiters per record
+    while(true)
+    {
+        recordsFile.seekg( (2 + RRN * 148), ios::beg);
 
         char tempISBN [6];
-        out.read(tempISBN , sizeof(tempISBN));
-        /*if we found the book we need to update the avail list
-         *
-         */
-        if(strcmp(isbn , tempISBN) == 0){
-            short nOFF =RRN;
-//            out.seekp(0 , ios::beg);
-            out.seekp(sizeof(short) + nOFF* sizeof(Book )+ 6*nOFF * sizeof(char)   , ios::beg);/// handling the new short written
-            /// new problem here
-            char del_Delimiter = '*';
-            out.write((char *)&del_Delimiter , sizeof(del_Delimiter));
-            updateAVAIL(out, nOFF);/// handle the deletion delimiter
-
+        recordsFile.read(tempISBN , sizeof(tempISBN));
+        if(strcmp(isbn , tempISBN) == 0) /// if we found the book we need to update the avail list
+        {
+            /**
+             * once the desired book has been found the following procedures are taken:
+             * 1- read the head of the AVAIL_LIST (located at the first 2 bytes of the file) and store them in currentHead (short)
+             * 2- update the head of the AVAIL_LIST with the byte-offset of the desired book, the byte-offset is stored in RRN (short)
+             * 3- add '*' and currentHead at the first 3 bytes of the record being deleted.
+             */
+            short currentHead; // -1
+            recordsFile.seekg(0, ios::beg);
+            recordsFile.read((char*) &currentHead, sizeof(currentHead)); /// read current head (located at the first 2 bytes of the file).
+            recordsFile.seekp(0, ios::beg);
+            recordsFile.write((char*) &RRN, sizeof(RRN)); /// update the head with the current index being deleted.
+            /**
+             * after updating the AVAIL_LIST the following procedures are taken:
+             * 1- seek byte-offset of book being deleted
+             * 2- write '*' on 1st byte
+             * 3- write currentHead at 3rd and 4th bytes.
+             * e.g. (ASSUME CURRENT HEAD = 3) BEFORE: isbn1 - AFTER: *3n1, "isb" has been replaced with *3
+             */
+            recordsFile.seekp((2 + RRN * 148), ios::beg);
+            char deletingChar = '*';
+            recordsFile.write(&deletingChar, sizeof(deletingChar));
+            recordsFile.write((char*) &currentHead, sizeof(currentHead));
             break;
         }
-        if(out.tellp() == ios::end) {
-            break;
-        }
-
+        else {++RRN;}
+        if(recordsFile.tellp() == ios::end) { break; }
     }
-
 }
 
-int main() {
-    freopen("in.txt","r",stdin);
+int main()
+{
     short head = -1;
-
     Book testBook;
     fstream recordsFile;
-    recordsFile.open("Hanfy.txt", ios::out | ios::in | ios::trunc); ///TODO make sure the program runs correctly in ios::app mode
+    recordsFile.open("records.txt", ios::out | ios::in);
     recordsFile.write((char*)&head, sizeof(head));
-    cin>>testBook;
-    addBook(recordsFile, testBook);
-//    return 0 ;
-//    deleteBook(recordsFile , "123");
-    recordsFile.seekg(0 ,ios::beg);
-    printBook(recordsFile,"123");
-    recordsFile.close();
+
 }
