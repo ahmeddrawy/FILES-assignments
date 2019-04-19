69b1ad022fceae76b60d71b2ea5e08c10bed2f9e
drawy <ahmedhanfy1111@gmail.com>
2019-04-19 09:37:32 +0200

    added the secondary and inverted list and getting record by id

--- Assignment_3_files/DataBase.h
+++ Assignment_3_files/DataBase.h
@@ -15,6 +15,7 @@ using namespace std;
 class DataBase {
 private:
     bool mIndxUpdated = false ;
+    int invertedRRN = 0 ;
     struct sDataRecord{
         char ID[6];
         char CouseName[51];
@@ -72,6 +73,11 @@ private:
 
 public:
     DataBase(){
+        this->readPrimary();
+        this->readSecondary();
+        this->readInverted();
+        sort(mPrimaryIndx.begin() , mPrimaryIndx.end());
+        sort(msSecondaryIndx.begin() , msSecondaryIndx.end());
 
     }
     void insertRecord(){
@@ -94,18 +100,76 @@ public:
         sPrimaryIndx tPrimary ;
         strcpy(tPrimary.ID , temp.ID);
         tPrimary.offset = address;
-
+        sInvertedList tInverted;
         mPrimaryIndx.push_back(tPrimary);
-
-
+        int indxOfSecondary =alreadyInSecondary(temp);
+        if(indxOfSecondary>= 0 ) {
+            int curr = msSecondaryIndx[indxOfSecondary ].InvertedListIndx;
+            while(curr != -1 ){
+                sInvertedList & node=  mInvertedList[curr];
+              curr = node.nxt;
+              if(curr == -1){
+                  node.nxt = invertedRRN++;
+                  sInvertedList iTemp;
+                  iTemp.nxt = -1 ;
+                  strcpy(iTemp.PrimaryIndx ,temp.ID);
+                  mInvertedList.push_back(iTemp);
+                  break;
+              }
+            }
+        }
+        else {
+            sSecondaryIndx tSecondary;
+            tSecondary.InvertedListIndx = invertedRRN++;
+            strcpy(tSecondary.InstructorName , temp.Instructor);
+            msSecondaryIndx.push_back(tSecondary);
+            strcpy(tInverted.PrimaryIndx,  temp.ID);
+            tInverted.nxt = -1;
+            mInvertedList.push_back(tInverted);
+        }
 
 
     }
-    void compare(){/// todo remove this
-        sort(mPrimaryIndx.begin() , mPrimaryIndx.end());
-        for (int i = 0; i <mPrimaryIndx.size() ; ++i) {
-            cout<<mPrimaryIndx[i].ID<<endl;
+
+    int alreadyInSecondary(sDataRecord & obj){
+        for (int i = 0; i <msSecondaryIndx.size() ; ++i) {
+            if(strcmp(msSecondaryIndx[i].InstructorName , obj.Instructor) == 0){
+                return i ;
+            }
         }
+        return -1 ;
+    }
+    int  nxtRRNinInverted(){
+        fstream out ; out.open(mInvertedListFilePath.c_str() , ios::out | ios :: binary);
+        out.seekg(0 , ios::end  );
+        auto it = out.tellg();
+        return it / sizeof(sInvertedList);
+    }
+//    void compare(){/// todo remove this
+//        sort(mPrimaryIndx.begin() , mPrimaryIndx.end());
+//        for (int i = 0; i <mPrimaryIndx.size() ; ++i) {
+//            cout<<mPrimaryIndx[i].ID<<endl;
+//        }
+//
+//    }
+    void getRecordById(){
+        char tempId[6];
+        cout<<"Please enter the ID you want to search for : ";
+        cin>>tempId;
+        int indx = this->BinarySearchPrimary(tempId);
+        if(indx != -1 ){
+            cout<<mPrimaryIndx[indx].ID<<" "<<mPrimaryIndx[indx].offset<<endl; /// todo remove this
+            sDataRecord temp = readRecord(mPrimaryIndx[indx].offset);
+            cout<<temp<<endl;
+        }
+        else {
+            cout<<"Record Not Found\n";
+        }
+
+    }
+    void getRecordByName(){
+        char Name [51];cin>>Name;
+
 
     }
     void WritePrimaryFile(){    /// todo 13/4/19 17:25,take care of the secondary indx and inverted list
@@ -114,18 +178,18 @@ public:
         }
         sort(mPrimaryIndx.begin() , mPrimaryIndx.end());
         fstream out ;
-        out.open (mPrimaryIndxPath.c_str() , ios::trunc | ios::beg);
+        out.open (mPrimaryIndxPath.c_str() , ios::trunc | ios::binary);
         for (int i = 0; i <mPrimaryIndx.size() ; ++i) {
             out.write((char *)&mPrimaryIndx[i] , sizeof(mPrimaryIndx[i]));
             sDataRecord toBeSearched = readRecord(mPrimaryIndx[i].offset);
             sSecondaryIndx tempSecondary ;
             strcpy(tempSecondary.InstructorName , toBeSearched.Instructor);
-//            tempSecondary.PrimaryIndx = i ; /// todo 
+//            tempSecondary.PrimaryIndx = i ; /// todo
             msSecondaryIndx.push_back(tempSecondary);
 
         }
     }
-    int BinarySearchPrimary(char * arr){
+    int BinarySearchPrimary(char * arr){        /// return -1 if not found and indx in primary vector otherwise
         if(!this->mIndxUpdated){ ///todo  construct the primary
 
         }
@@ -170,7 +234,79 @@ public:
         out<<state;
         out.close();
     }
+    void writeVectorsToFiles() {
+        sort(mPrimaryIndx.begin() , mPrimaryIndx.end());
+        sort(msSecondaryIndx.begin() , msSecondaryIndx.end());
 
+        for (int i = 0; i < mPrimaryIndx.size(); ++i) {
+            writePrimary(mPrimaryIndx[i]);
+        }
+        for (int j = 0; j <msSecondaryIndx.size() ; ++j) {
+            writeSecondary(msSecondaryIndx[j]);
+        }
+        for (int k = 0; k <mInvertedList.size() ; ++k) {
+            writeInverted(mInvertedList[k]);
+        }
+    }
+    void writePrimary(sPrimaryIndx & obj){
+        fstream out ; out.open(mPrimaryIndxPath.c_str() ,ios::app| ios:: out | ios::binary);
+        out.seekp(0, ios::end);
+        out.write((char *) &obj , sizeof(obj));
+        out.close();
+    }
+    void writeSecondary(sSecondaryIndx & obj){
+        fstream out ; out.open(mSecondaryIndxFilePath.c_str() , ios::app|ios:: out | ios:: binary);
+        out.seekp(0, ios::end);
+        out.write((char *)& obj , sizeof(obj));
+        out.close();
+    }
+    void writeInverted(sInvertedList &obj){
+        fstream out ; out.open(mInvertedListFilePath.c_str() ,ios::app| ios::out | ios ::binary);
+        out.seekp(0, ios::end);
+        out.write((char *)& obj , sizeof(obj));
+        out.close();
+    }
+    void readPrimary() {
+        ifstream out;
+        out.open(mPrimaryIndxPath.c_str());
+        while (true) {
+            if (out.eof()) break;
+            sPrimaryIndx temp;
+            out.read((char *) &temp, sizeof(temp));
+            if(out.eof()) break;
+            mPrimaryIndx.push_back(temp);
+//            cout << temp.ID << " " << temp.offset << endl;
+        }
+        out.clear();
+        out.close();
+    }
+    void readSecondary() {
+        ifstream out;
+        out.open(mSecondaryIndxFilePath.c_str());
+        while (true) {
+            if (out.eof()) break;
+            sSecondaryIndx temp;
+            out.read((char *) &temp, sizeof(temp));
+            if(out.eof()) break;
+            msSecondaryIndx.push_back(temp);
+//            cout << temp.InvertedListIndx << " " << temp.InstructorName << endl;
+        }
+        out.clear();
+        out.close();
+    }
+    void readInverted(){
+        ifstream out ; out.open(mInvertedListFilePath.c_str() );
+        while(true) {
+            if(out.eof()) break;
+            sInvertedList temp;
+            out.read((char *)&temp, sizeof(temp));
+            if(out.eof()) break;
+            mInvertedList.push_back(temp);
+//            cout << temp.PrimaryIndx << " " << temp.nxt << endl;
+        }
+        out.clear();
+        out.close();
+    }
 
 };
 
--- Assignment_3_files/main.cpp
+++ Assignment_3_files/main.cpp
@@ -3,10 +3,18 @@
 int main() {
 //   freopen("input.txt", "r" ,stdin);
     DataBase m ;
+    m.getRecordById();
+//    m.insertRecord();
+//    m.insertRecord();
+//    m.insertRecord();
+//    m.insertRecord();
+//    m.writeVectorsToFiles();
 //    m.compare();
-    m.insertRecord();
-    m.insertRecord();
-    m.compare();
+//    m.readPrimary();
+//    puts("-----");
+//    m.readSecondary();
+//    puts("-----");
+//    m.readInverted();
 //  m.readRecord(1);
 //    char ar[3]= "32";
 //    cout<<atoi(ar) +101;
